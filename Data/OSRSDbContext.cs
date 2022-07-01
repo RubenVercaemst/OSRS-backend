3791411016c89249cb44305126460db4efbcac10
RubenVercaemst <ruben.vercaemst@gmail.com>
2022-07-01 18:11:53 +0200

    Implemented Identity

--- Data/OSRSDbContext.cs
+++ Data/OSRSDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OSRSAPI.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace OSRSAPI.Data
 {
-    public class OSRSDbContext: DbContext
+    public class OSRSDbContext: IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
     {
         public OSRSDbContext(DbContextOptions options) : base(options)
         {
--- OSRSAPI/Program.cs
+++ OSRSAPI/Program.cs
@@ -1,15 +1,27 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using OSRSAPI.Data;
+using OSRSAPI.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//configure DbContext
 builder.Services.AddDbContext<OSRSDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+//configure Identity
+builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(config =>
+{
+    config.SignIn.RequireConfirmedAccount = false;
+    config.User.RequireUniqueEmail = true;
+}).AddDefaultTokenProviders().AddEntityFrameworkStores<OSRSDbContext>();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
